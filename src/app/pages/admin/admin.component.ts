d3663c82e358e6c2473f64514118ef74e0afdaac
Ruth <ruthmarilsa20@gmail.com>
2026-04-26 22:01:13 +0200

    Update App

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; // Añadimos OnInit
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http'; // Importante para el ping
 import { MenuBarComponent } from '../../components/menu-bar/menu-bar.component';
 
 @Component({
@@ -9,10 +10,28 @@ import { MenuBarComponent } from '../../components/menu-bar/menu-bar.component';
   imports: [CommonModule, RouterModule, MenuBarComponent],
   templateUrl: './admin.component.html'
 })
-export class AdminComponent {
-  constructor(private router: Router) {}
+export class AdminComponent implements OnInit { // Implementamos OnInit
+
+  constructor(
+    private router: Router,
+    private http: HttpClient // Inyectamos HttpClient
+  ) {}
+
+  ngOnInit() {
+    // URL de tu servidor en Render
+    const urlPing = 'https://backend-ruth-slam.onrender.com/api/usuarios/ping';
+    https://backend-ruth-slam.onrender.com/api/usuarios/ping
+
+    console.log('--- Despertando Servidor ---');
+
+    // Petición silenciosa para que Render empiece a arrancar (los 2 min de espera)
+    this.http.get(urlPing, { responseType: 'text' }).subscribe({
+      next: () => console.log('✅ Servidor activo'),
+      error: () => console.log('⏳ Servidor despertando en segundo plano...')
+    });
+  }
 
   go(path: string) {
-   this.router.navigate([path]);
+    this.router.navigate([path]);
   }
 }

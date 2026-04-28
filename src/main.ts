cafa0e0f821b6bc706b77da4c0d8b1d97f24e3cb
Ruth <ruthmarilsa20@gmail.com>
2026-04-28 13:13:38 +0200

    Update App

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 
+// --- PARCHE PARA SOCKJS (ARREGLA LA PANTALLA NEGRA) ---
+(window as any).global = window;
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
+// ------------------------------------------------------
+
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),

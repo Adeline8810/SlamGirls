deeb92d2d5ad532142e351b3b1e26f5e44aab5af
Ruth <ruthmarilsa20@gmail.com>
2026-04-28 13:19:41 +0200

    Update App

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 
 // --- PARCHE PARA SOCKJS (ARREGLA LA PANTALLA NEGRA) ---
+// Define 'global' y 'process' antes de que cargue cualquier otra cosa
 (window as any).global = window;
 (window as any).process = {
   env: { DEBUG: undefined },

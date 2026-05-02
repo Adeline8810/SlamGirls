74f56fa24e29ad1b15b2904e7491d8be495c715a
Ruth <ruthmarilsa20@gmail.com>
2026-05-03 01:07:17 +0200

    Update App

--- src/app/components/inicio/inicio.ts
+++ src/app/components/inicio/inicio.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit, inject } from '@angular/core'; // 1. Importa 'inject'
+import { CommonModule } from '@angular/common';
 import { LiveService } from '../../../services/live.service';
 import { VideoService } from '../../../services/video.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
+  standalone: true, // Asegúrate de que esto esté así si usas imports aquí
+  imports: [RouterModule, CommonModule], // 2. Agrégalo aquí
   templateUrl: './inicio.html',
   styleUrls: ['./inicio.css']
 })
 export class Inicio implements OnInit {
   listaLives: any[] = [];
   listaVideos: any[] = [];
+  usuarioId: string = localStorage.getItem('usuarioId') || '0';
 
   // 2. Inyecta los servicios directamente aquí (forma moderna)
   private liveService = inject(LiveService);

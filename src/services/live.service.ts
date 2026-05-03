ce82957e7c6571d9c9f2c2a045428c25d5e3ab0b
Ruth <ruthmarilsa20@gmail.com>
2026-05-03 02:19:33 +0200

    Update App

--- src/services/live.service.ts
+++ src/services/live.service.ts
@@ -13,20 +13,20 @@ export class LiveService {
   constructor(private http: HttpClient) { }
 
   // 1. Obtener la lista de personas transmitiendo
-  getLivesActivos(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.api}/activos`);
-  }
-
+getLivesActivos(): Observable<any[]> {
+  return this.http.get<any[]>(`${this.api}/activos`);
+}
   // 2. Crear un nuevo live (Inicia la transmisión en la DB)
 // 1. Para empezar el live
 iniciarLive(usuarioId: number, titulo: string): Observable<any> {
   return this.http.post<any>(`${this.api}/iniciar?usuarioId=${usuarioId}&titulo=${titulo}`, {});
 }
 
-// 2. Para terminar el live (usando el usuarioId como referencia)
+
+
+// 2. Finalizar (Coincide con el nuevo método que pusimos arriba)
 finalizarLive(usuarioId: number): Observable<any> {
-  // Nota: Si tu backend pide el ID del LIVE en lugar del usuario,
-  // asegúrate de que el endpoint sea el correcto.
-  return this.http.post<any>(`${this.api}/finalizar/por-usuario/${usuarioId}`, {});
+  return this.http.post<any>(`${this.api}/finalizar/usuario/${usuarioId}`, {});
 }
+
 }

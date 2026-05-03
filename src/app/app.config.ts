8c4e68f4bdbd605b644713555e561ecaf649dfe7
Ruth <ruthmarilsa20@gmail.com>
2026-05-03 23:31:06 +0200

    Update App

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,22 +1,24 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core'; // 👈 IMPORTADO importProvidersFrom
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
-import { provideHttpClient,withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 
-
-
-// IMPORTS DE FIREBASE
+// FIREBASE
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore'; // 👈 AÑADIDO
 import { environment } from '../environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withHashLocation()), // Esto es lo más importante
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(withFetch()),
     provideAnimationsAsync(),
+
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth())
+    provideAuth(() => getAuth()),
+
+    provideFirestore(() => getFirestore()) // 👈 ESTO FALTABA
   ]
 };

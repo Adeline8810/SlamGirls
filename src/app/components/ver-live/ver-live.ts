3ed763b51c2eaf366fc6ac287e0cf0bf9bbf3271
Ruth <ruthmarilsa20@gmail.com>
2026-05-05 16:22:09 +0200

    Update App

--- src/app/components/ver-live/ver-live.ts
+++ src/app/components/ver-live/ver-live.ts
@@ -103,7 +103,7 @@ llamarAlEmisor() {
   console.log('Llamando a Adeline (ID: ' + this.userId + ')...');
 
   // Ruth inicia la llamada enviando un stream vacío
-  const call = this.peer.call(this.userId, new MediaStream());
+ const call = this.peer.call(this.userId, undefined as any);
 
   if (call) {
     // ESTO ES LO QUE FALTA: Ruth debe estar lista para recibir
